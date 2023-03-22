342c90c2b3d407050ce2c0b429b4407a8c817851
ibro001 <ibrahimmoses56@gmail.com>
2023-03-22 21:07:02 +0100

    completed the brand component

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './brand.css';
+import {google, slack, atlassian, dropbox, shopify } from './imports';
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='gpt3__brand section__padding'>
+      <div> <img src= {google} alt="google_logo"/> </div>
+      <div> <img src= {slack} alt="google_logo"/> </div>
+      <div> <img src= {atlassian} alt="google_logo"/> </div>
+      <div> <img src= {dropbox} alt="google_logo"/> </div>
+      <div> <img src= {shopify} alt="google_logo"/> </div>
+    </div>
   )
 }
 
