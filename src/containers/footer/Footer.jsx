42539457efb2e58ccf91d400e32a454c67531afc
ibro001 <ibrahimmoses56@gmail.com>
2023-03-27 22:01:07 +0100

    completed the footer component

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
+import gpt3Logo from '../../assets/logo.svg';
 import './footer.css';
+
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='gpt3__footer section__padding'>
+      <div className='gpt3__footer-heading'>
+        <h1 className='gradient__text'>
+          Do you want to step into the future before others?
+        </h1>
+      </div>
+      <div className='gpt3__footer-btn'>
+        <p>Request Early Access</p>
+      </div>
+      <div className='gpt3__footer-links'>
+        <div className='gpt3__footer-links_logo'>
+          <img src={gpt3Logo} alt="logo" />
+          <p>
+            250T, Ahmadu Bello Way, Victoria Island Lagos.
+          </p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contacts</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contacts</p>
+        </div>
+        <div className='gpt3__footer-links_div'>
+          <h4>Get in touch</h4>
+          <p>250T, Ahmadu Bello Way, Victoria Island Lagos.</p>
+          <p>085-132567</p>
+          <p>info@payme.net</p>
+        </div>
+      </div>
+      <div className='gpt3__footer-copyright'>
+        <p>©2023 GPT-3, All rights reserved.</p>
+      </div>
+    </div>
   )
 }
 
